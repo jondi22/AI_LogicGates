b29c0323c2c18765b2e124166f07a54fa568625a
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2022-12-29 20:56:56 +0300

    update datasets correct neural network

--- src/main/java/ai_control/DataSets.java
+++ src/main/java/ai_control/DataSets.java
@@ -24,6 +24,21 @@ public class DataSets {
         Double [] tempDataTuple3 = {1.0, 1.0, 1.0};
         AND_DATA.add(tempDataTuple3);
 
+        Double [] NOT = { 0.0, 1.0};
+        NOT_DATA.add(NOT);
+        Double [] NOT1 = { 1.0, 0.0};
+        NOT_DATA.add(NOT1);
+
+
+        Double [] var1 = {0.0, 0.0, 1.0};
+        NAND_DATA.add(var1);
+        Double [] ver2 = {0.0, 1.0, 1.0};
+        NAND_DATA.add(ver2);
+        Double [] var3 = {1.0, 0.0, 1.0};
+        NAND_DATA.add(var3);
+        Double [] var4 = {1.0, 1.0, 0.0};
+        NAND_DATA.add(var4);
+
         Double [] tempDataTupleo = {0.0, 0.0, 0.0};
         OR_DATA.add(tempDataTupleo);
         Double [] tempDataTuple1o = {0.0, 1.0, 1.0};
@@ -32,6 +47,36 @@ public class DataSets {
         OR_DATA.add(tempDataTuple2o);
         Double [] tempDataTuple3o = {1.0, 1.0, 1.0};
         OR_DATA.add(tempDataTuple3o);
+
+
+        Double [] s1 = {0.0, 0.0, 1.0};
+        NOR_DATA.add(s1);
+        Double [] s12 = {0.0, 1.0, 0.0};
+        NOR_DATA.add(s12);
+        Double [] s122 = {1.0, 0.0, 0.0};
+        NOR_DATA.add(s122);
+        Double [] s13 = {1.0, 1.0, 0.0};
+        NOR_DATA.add(s13);
+
+        Double [] xnor1 = {0.0, 0.0, 1.0};
+        XNOR_DATA.add(xnor1);
+        Double [] xnor2 = {0.0, 1.0, 0.0};
+        XNOR_DATA.add(xnor2);
+        Double [] xnor12 = {1.0, 0.0, 0.0};
+        XNOR_DATA.add(xnor12);
+        Double [] xnor13 = {1.0, 1.0, 1.0};
+        XNOR_DATA.add(xnor13);
+
+
+        Double [] xor1 = {0.0, 0.0, 0.0};
+        XOR_DATA.add(xor1);
+        Double [] xor2 = {0.0, 1.0, 1.0};
+        XOR_DATA.add(xor2);
+        Double [] xor3 = {1.0, 0.0, 1.0};
+        XOR_DATA.add(xor3);
+        Double [] xor4 = {1.0, 1.0, 0.0};
+        XOR_DATA.add(xor4);
+
     }
 
 
--- src/main/java/ai_control/Network2Input1Hidden.java
+++ src/main/java/ai_control/Network2Input1Hidden.java
@@ -8,78 +8,101 @@ import java.util.ArrayList;
 
 public class Network2Input1Hidden {
     private Neuron outputNeuron;
-    private Neuron [] hidden = new Neuron[2];
+    private Neuron[] hidden = new Neuron[2];
     private int noOfEpochsInLastTrain;
+    private ActivationFunctionInterface outputActivationFunction;
+    private MultiLayerActivationFunctionInterface hiddenLayerActivFunction;
+    private int outThresh;
+    private int hiddenThresh;
+
 
     public Network2Input1Hidden(MultiLayerActivationFunctionInterface hiddenLayerActivFunction,
-                                ActivationFunctionInterface outputActivationFunction, int hiddenThresh, int outThresh){
+                                ActivationFunctionInterface outputActivationFunction, int hiddenThresh, int outThresh) {
         outputNeuron = new Neuron(outputActivationFunction, 2, outThresh);
         for (int i = 0; i < 2; i++) {
             hidden[i] = new Neuron(hiddenLayerActivFunction, 2, hiddenThresh);
         }
+        this.outputActivationFunction = outputActivationFunction;
+        this.hiddenLayerActivFunction = hiddenLayerActivFunction;
+        this.hiddenThresh = hiddenThresh;
+        this.outThresh = outThresh;
     }
-    public void train(ArrayList<Double[]> dataSet, double learningRate){
-        if(dataSet.size() <= 0) throw new IllegalArgumentException("dataset is empty");
-        else if(dataSet.get(0).length != 3)
+
+    public void train(ArrayList<Double[]> dataSet, double learningRate) {
+        if (dataSet.size() <= 0) throw new IllegalArgumentException("dataset is empty");
+        else if (dataSet.get(0).length != 3)
             throw new IllegalArgumentException("data doesn't suite the perceptron");
 
         this.noOfEpochsInLastTrain = 0;
-        double [] tempForTrain = new double[2];
-        double [] tempModifications = new double[2];
-        while(true){
-            double SSE = 0;
-            System.out.println("Epoch No. " + (this.noOfEpochsInLastTrain + 1));
-            for(int i = 0; i < dataSet.size(); i++){
-                Double [] dataRow = dataSet.get(i);
-                for(int j = 0; j < 2 ; j++){
-                    tempForTrain[j] = dataRow[j];
-                }
-                double [] out = new double[2];
-                double [] outDerivative = new double[2];
-                for (int j = 0; j < 2; j++) {
-                    out[j] = hidden[j].getOutput(tempForTrain);
-                    outDerivative[j] = hidden[j].getDerivedOutput(tempForTrain);
-                }
-                double finalOut = outputNeuron.getOutput(out);
-                double error = dataRow[2] - finalOut;
-                if(error != 0) {
+        double[] tempForTrain = new double[2];
+        int flag = 0;
+        while (true) {
+            noOfEpochsInLastTrain = 0;
+            hidden = new Neuron[2];
+            outputNeuron = new Neuron(outputActivationFunction, 2, outThresh);
+            for (int i = 0; i < 2; i++) {
+                hidden[i] = new Neuron(hiddenLayerActivFunction, 2, hiddenThresh);
+            }
 
-                    double[] outWeightModifications = new double[2];
-
+            while (this.noOfEpochsInLastTrain < 1000) {
+                double SSE = 0;
+                System.out.println("Epoch No. " + (this.noOfEpochsInLastTrain + 1));
+                for (int i = 0; i < dataSet.size(); i++) {
+                    Double[] dataRow = dataSet.get(i);
                     for (int j = 0; j < 2; j++) {
-                        outWeightModifications[j] = learningRate * out[j] * error;
+                        tempForTrain[j] = dataRow[j];
                     }
-
-                    double outThreshUpdate = learningRate * outputNeuron.getThreshold();
-                    double[][] nodeModifications = new double[2][2];
-                    double[] threshModifications = new double[2];
+                    double[] out = new double[2];
+                    double[] outDerivative = new double[2];
                     for (int j = 0; j < 2; j++) {
-                        for (int k = 0; k < 2; k++) {
-                            nodeModifications[j][k] = learningRate * tempForTrain[k] *
+                        out[j] = hidden[j].getOutput(tempForTrain);
+                        outDerivative[j] = hidden[j].getDerivedOutput(tempForTrain);
+                    }
+                    double finalOut = outputNeuron.getOutput(out);
+                    double error = dataRow[2] - finalOut;
+                    if (error != 0) {
+                        SSE = error * error;
+
+                        double[] outWeightModifications = new double[2];
+
+                        for (int j = 0; j < 2; j++) {
+                            outWeightModifications[j] = learningRate * out[j] * error;
+                        }
+
+                        double outThreshUpdate = learningRate * outputNeuron.getThreshold();
+                        double[][] nodeModifications = new double[2][2];
+                        double[] threshModifications = new double[2];
+                        for (int j = 0; j < 2; j++) {
+                            for (int k = 0; k < 2; k++) {
+                                nodeModifications[j][k] = learningRate * tempForTrain[k] *
+                                        (outDerivative[j] * outputNeuron.getWeights().get(j));
+                            }
+                            threshModifications[j] = learningRate * hidden[j].getThreshold() *
                                     (outDerivative[j] * outputNeuron.getWeights().get(j));
                         }
-                        threshModifications[j] = learningRate * hidden[j].getThreshold() *
-                                (outDerivative[j] * outputNeuron.getWeights().get(j));
+                        for (int j = 0; j < 2; j++) {
+                            hidden[j].updateValues(threshModifications[j], nodeModifications[j]);
+                        }
+                        outputNeuron.updateValues(outThreshUpdate, outWeightModifications);
                     }
-                    for (int j = 0; j < 2; j++) {
-                        hidden[i].updateValues(threshModifications[j], nodeModifications[j]);
-                    }
-                    outputNeuron.updateValues(outThreshUpdate, outWeightModifications);
+
+
                 }
-
-
+                this.noOfEpochsInLastTrain++;
+                System.out.println(SSE);
+                if ((SSE / dataSet.size()) <= 0.1) {
+                    flag = 1;
+                    break;
+                }
             }
-            this.noOfEpochsInLastTrain++;
-            System.out.println(SSE);
-            if((SSE / dataSet.size()) <= 0.1) break;
+            if (flag != 0) break;
         }
-
 
     }
 
-    public double getOutput(double ...inputs){
-        if(inputs.length != 2) throw new IllegalArgumentException();
-        double [] out = new double[2];
+    public double getOutput(double... inputs) {
+        if (inputs.length != 2) throw new IllegalArgumentException();
+        double[] out = new double[2];
         for (int j = 0; j < 2; j++) {
             out[j] = hidden[j].getOutput(inputs);
         }
